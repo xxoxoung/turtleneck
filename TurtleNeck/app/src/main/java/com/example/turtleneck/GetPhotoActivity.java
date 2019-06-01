9d1a49209755335b542aa3f825989de7fdd53de2
soyoung <kosoro123@gmail.com>
2019-06-02 02:38:09 +0900

    팝업으로 다시 만든 것

--- TurtleNeck/app/src/main/java/com/example/turtleneck/GetPhotoActivity.java
+++ TurtleNeck/app/src/main/java/com/example/turtleneck/GetPhotoActivity.java
@@ -1,14 +1,20 @@
 package com.example.turtleneck;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class GetPhotoActivity extends MainActivity implements View.OnClickListener {
+import java.io.InputStream;
+
+public class GetPhotoActivity extends MainActivity {
     ImageView imageView;
     Button Getpht;
 
@@ -21,15 +27,68 @@ public class GetPhotoActivity extends MainActivity implements View.OnClickListen
 
         Getpht.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 1);
+                // 카메라로
+                DialogInterface.OnClickListener gocam = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent1, 1);
+                    }
+                };
+
+                // 갤러리로
+                DialogInterface.OnClickListener gogal = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent2 = new Intent();
+                        intent2.setType("image/*");
+                        intent2.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(intent2, 2);
+                    }
+                };
+
+                //취소
+                DialogInterface.OnClickListener can = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                };
+
+                new AlertDialog.Builder(GetPhotoActivity.this)
+                        .setTitle("사진을 골라주세요!")
+                        .setPositiveButton("카메라로", gocam)
+                        .setNeutralButton("취소", can)
+                        .setNegativeButton("갤러리로", gogal)
+                        .show();
             }
         });
     }
 
-    protected void onActivityReslut(int requestcode, int resultcode, Intent data) {
-        if(requestcode == 1) {
-            imageView.setImageURI(data.getData());
+    protected void onActtivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            if(requestCode == 1) {
+                try {
+                    Bundle bundle = data.getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                    Log.i("카메라 사진","img" );
+                    imageView.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(requestCode == 2) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    Log.i("갤러리 사진","img" );
+                    imageView.setImageBitmap(img);
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
