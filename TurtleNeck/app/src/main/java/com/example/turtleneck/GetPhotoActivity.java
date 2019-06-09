f137fa183996750b2cdbfd91acecc94de852290c
soyoung <kosoro123@gmail.com>
2019-06-09 14:26:28 +0900

    사진 저장, 불러오기 완료

--- TurtleNeck/app/src/main/java/com/example/turtleneck/GetPhotoActivity.java
+++ TurtleNeck/app/src/main/java/com/example/turtleneck/GetPhotoActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -88,7 +90,31 @@ public class GetPhotoActivity extends MainActivity {
                         Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
 
                         if(bitmap != null) {
-                            imageView.setImageBitmap(bitmap);
+                            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+                            // 회전된 사진
+                            Bitmap rotatedBitmap = null;
+                            switch (orientation) {
+                                case ExifInterface.ORIENTATION_ROTATE_90 : {
+                                    rotatedBitmap = rotateImage(bitmap, 90);
+                                    break;
+                                }
+                                case ExifInterface.ORIENTATION_ROTATE_180 : {
+                                    rotatedBitmap = rotateImage(bitmap, 180);
+                                    break;
+                                }
+                                case ExifInterface.ORIENTATION_ROTATE_270 : {
+                                    rotatedBitmap = rotateImage(bitmap, 270);
+                                    break;
+                                }
+                                case ExifInterface.ORIENTATION_NORMAL :
+                                    default:
+                                        rotatedBitmap = bitmap;
+                            }
+
+                            // 이미지뷰에 사진 띄우기
+                            imageView.setImageBitmap(rotatedBitmap);
                         }
                     }
                     break;
@@ -102,16 +128,16 @@ public class GetPhotoActivity extends MainActivity {
     // 카메라 실행
     private void dispatchTakePictureIntent() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
+        // 인텐트 확인
         if (intent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
+            // 파일 만들고 초기화
             File photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
-                // Error occurred while creating the File
+                // 에러난 경우
             }
-            // Continue only if the File was successfully created
+            // 파일이 정상적으로 만들어지면 계속
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "com.example.turtleneck.fileprovider",
@@ -136,5 +162,12 @@ public class GetPhotoActivity extends MainActivity {
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    // 사진 돌려주는 함수
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 }
