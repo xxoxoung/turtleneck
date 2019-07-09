65682128d51c1e6d4e7ca98055dc60dd62ad27ba
soyoung <kosoro123@gmail.com>
2019-07-09 19:11:53 +0900

    네비바 추가중

--- turtleon-sso/TurtleNeck/app/src/main/java/com/example/turtleneck/MainActivity.java
+++ turtleon-sso/TurtleNeck/app/src/main/java/com/example/turtleneck/MainActivity.java
@@ -1,14 +1,120 @@
 package com.example.turtleneck;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 
-// 메인 화면
-public class MainActivity extends AppCompatActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 우측 하단 메일 표시
+        // 나중에 삭제 필요
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        // 우측 상단 점 세개 버튼
+        // 나중에 삭제 필요
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_StartDia) {
+            // 진단 시작 화면으로 이동
+            Intent intent1 = new Intent(this, DiagActivity.class);
+            startActivity(intent1);
+            finish();
+        } else if (id == R.id.nav_ConfirmDia) {
+            // 진단 확인 화면으로 이동
+
+        } else if (id == R.id.nav_ModifyDia) {
+            // 진단 정보 수정 화면으로 이동
+
+        } else if (id == R.id.nav_ModifySIgn) {
+            // 회원 정보 수정 화면으로 이동
+
+        } else if (id == R.id.nav_Board) {
+            // 게시글 관리 화면으로 이동
+
+        } else if (id == R.id.nav_Setting) {
+            // 설정 화면으로 이동
+
+        } else if (id == R.id.nav_Send) {
+            // 오류보내기 화면으로 이동
+
+        } else if (id == R.id.nav_FAQ) {
+            // 고객센터 화면으로 이동
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
